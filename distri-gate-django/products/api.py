61fa347f1972fcf18c798fca26023b33e8e51052
Van Salido <vansalido@gmail.com>
2024-02-01 07:53:24 +0800

    refactor: used function from utils

--- distri-gate-django/products/api.py
+++ distri-gate-django/products/api.py
@@ -6,11 +6,11 @@ from users.models import User
 from products.models import Product, ProductVariant
 from products.schemas import ProductSchema, ProductVariantSchema
 from products.schemas import ProductInput, VariantInput
-from products.utils import transform_product_input_data
+from products.utils import transform_product_input_data, save_new_product
 
 from distrigate.security import require_token
-from django.http import HttpRequest
-from django.core.files.base import ContentFile
+from django.http import HttpRequest, JsonResponse
+
 
 from io import BytesIO
 
@@ -34,7 +34,7 @@ def get_product(request:HttpRequest,id:int):
 
 
 @app.post('add_new_product', response=list[ProductSchema])
-# @require_token
+@require_token
 def add_new_product(request:HttpRequest):
     
     
@@ -43,35 +43,12 @@ def add_new_product(request:HttpRequest):
     # user:User = request.user
     user = User.objects.first()
 
-    new_product = Product.objects.create(
-        title = product_input.title,
-        category = product_input.category,
-        seller = user
-    )
+    if not user.is_superuser:
+        return JsonResponse({'message': 'Must be a superuser to access'}, status=401)
 
-    for variant in product_input.variations:
-        # Initiating new variant
-        new_variant = ProductVariant.objects.create(
-            group = new_product,
-            type  = variant.type,
-            name  = variant.name,
-            variant_description = variant.variation_description,
-            price_amount  = variant.price_amount,
-            price_currency_code    = variant.price_currency_code,
-            price_currency_symbol  = variant.price_currency_symbol
-        )
-   
-        # Add the image to the variant
-        if variant.variant_image != b'':
-            new_variant.variant_image.save(
-                variant.variant_image_name,
-                ContentFile(variant.variant_image,variant.variant_image_name),
-                save=True)
-
-        # Add variant to product
-        new_product.variations.add(new_variant)
+    product = save_new_product(product_input,user)
     
-    new_product.save()
+    
     return Product.objects.all()
 
     
