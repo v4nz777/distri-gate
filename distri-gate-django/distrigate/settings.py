f65612347f9e707847e8ca965bfb21fdf09e9d59
Van Salido <vansalido@gmail.com>
2024-06-30 22:30:02 +0800

    x

--- distri-gate-django/distrigate/settings.py
+++ distri-gate-django/distrigate/settings.py
@@ -161,7 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS HEADERS OPTION
 # 
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://distri-gate-front.vercel.app']
 
 CORS_ALLOW_METHODS = (
     "DELETE",
